b5e19e98a662e07868297cee330f6407b8b141f7
mutantEris <mmires@live.com>
2023-04-01 17:45:03 -0500

    Lights And Pretty Stuff

--- MLStreelights/Assets/Scripts/manager.cs
+++ MLStreelights/Assets/Scripts/manager.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class manager : MonoBehaviour
 {
     public List<GameObject> car_prefabs = new();
-    public List<GameObject> spawn_points = new();
-    public List<string> directions = new() {"Left", "Forward", "Right"};
-    public int carSpeedMin;
-    public int carSpeedMax;
-    
     // Start is called before the first frame update
     void Start()
     {
+
         foreach (GameObject car in Resources.LoadAll("Prefabs"))
             car_prefabs.Add(car);
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            spawn_points[0].GetComponent<SpawnPoint>().SpawnCar();
+        
     }
 }
