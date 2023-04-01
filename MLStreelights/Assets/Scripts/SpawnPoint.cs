b13917aa1aaef7b30a080e3eda7d261b9acb3c91
Isaac Green <zuderg@gmail.com>
2023-04-01 15:08:21 -0500

    started car spawners

--- MLStreelights/Assets/Scripts/SpawnPoint.cs
+++ MLStreelights/Assets/Scripts/SpawnPoint.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
+    public GameObject manager;
+    public List<GameObject> car_prefabs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        manager = GameObject.Find("manager");
+        car_prefabs = manager.GetComponent<manager>().car_prefabs;
     }
 
     // Update is called once per frame
@@ -16,8 +19,9 @@ public class SpawnPoint : MonoBehaviour
         
     }
 
-    GameObject SpawnCar()
+    public GameObject SpawnCar()
     {
-        return(null);
+        GameObject car = Instantiate(car_prefabs[Random.Range(0, car_prefabs.Count - 1)]);
+        return car;
     }
 }
